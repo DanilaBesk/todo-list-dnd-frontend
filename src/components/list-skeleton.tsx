49d67ea9dd7f9aa4ed62783861cbc6d6910424c0
DanilaBesk <danilbeskorovaev546@gmail.com>
2024-01-10 02:16:58 +0300

    refactor: change classes in list-skeleton

--- src/components/list-skeleton.tsx
+++ src/components/list-skeleton.tsx
@@ -1,7 +1,7 @@
 import { Skeleton } from '../components/ui/skeleton';
 export const ListSkeleton = () => {
   return (
-    <div className="flex flex-col items-center h-[400px] w-full gap-y-2 p-1 pt-1.5">
+    <div className="space-y-2 items-center h-[70vh] w-[256px] p-1 pt-1.5">
       <Skeleton className="h-9 w-full rounded-md bg-slate-700" />
       <Skeleton className="h-9 w-full rounded-md bg-slate-700" />
       <Skeleton className="h-9 w-full rounded-md bg-slate-700" />
